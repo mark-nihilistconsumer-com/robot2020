2537db516d5cf00599a768c4ce26616ee3409593
daria-nihilistconsumer-com <75666935+daria-nihilistconsumer-com@users.noreply.github.com>
2021-01-12 23:02:45 +1100

    Meet people, shoot specific people

--- main.ts
+++ main.ts
@@ -1,56 +1,68 @@
-namespace BubbaBlocks {
+namespace CardboardRobotBlocks {
 
-    let onShotCallbacks: ((shooter: Number) => void)[] = []
-    let onResurrectionCallbacks: (() => void)[] = []
-    let knownPeople: Number[] = []
+    let _onShotCallbacks: ((shooter: number) => void)[] = []
+    let _onMeetNewRobotCallbacks: ((robot: number) => void)[] = []
+    let _knownRobots: number[] = []
 
 
     //% block="On I Got Shot"
-    export function addOnShotHandler(onshot: (shooter: Number) => void): void {
-        onShotCallbacks.push(onshot)
+    export function addOnShotHandler(onshot: (shooter: number) => void): void {
+        _onShotCallbacks.push(onshot)
     };
 
-    //% block="On Resurrection"
-    export function addOnResurrectionHandler(onResurrected: () => void): void {
-        onResurrectionCallbacks.push(onResurrected)
+    //% block="On Meet New Robot"
+    export function addOnMeetNewRobotHandler(onMeetNewRobot: (robot: number) => void): void {
+        _onMeetNewRobotCallbacks.push(onMeetNewRobot)
     };
 
-    //% block="Fire Gun"
-    export function fireGun(): void {
-        radio.sendString("BANG")
+    //% block="Shoot Robot $robot"
+    export function shoot(robot: number): void {
+        radio.sendValue("BANG", robot)
     }
 
+    //% block="Introduce Self"
     export function introduceSelf(): void {
         radio.sendString("HELLO")
     }
-    
-    export function getShot(shooter: Number): void {
-        for (let onShotHandler of onShotCallbacks) {
-            onShotHandler(shooter)
-        }
-        for (let onRessurectionHandler of onResurrectionCallbacks) {
-          onRessurectionHandler()
-        }
-        
-    }
 
-    export function meetPerson(person: Number): void {
-        knownPeople.push(person)
-        basic.showString("I met: " + person.toString())
+    //% block="Known Robots"
+    export function knownRobots(): number[] {
+        return _knownRobots.slice()
     }
 
     //% block="Join Game"
     export function joinGame(): void {
+
+        radio.onReceivedValue(function (name: string, value: number) {
+            let sender = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+            if (name == "BANG") {
+                if (value == control.deviceSerialNumber()) {
+                    getShot(sender)
+                }
+            }
+        })
+
         radio.onReceivedString(function (receivedString: string) {
             let sender = radio.receivedPacket(RadioPacketProperty.SerialNumber)
-            if (receivedString == "BANG") {
-                getShot(sender)
-            }
             if (receivedString == "HELLO") {
-                meetPerson(sender)
+                meetNewRobot(sender)
             }
         })
         radio.setTransmitSerialNumber(true)
-        introduceSelf()
+    }
+    
+    function getShot(shooter: number): void {
+        for (let onShotHandler of _onShotCallbacks) {
+            onShotHandler(shooter)
+        }
+    }
+
+    function meetNewRobot(robot: number): void {
+        if (_knownRobots.indexOf(robot) != -1) {
+            _knownRobots.push(robot)
+            for (let onMeetNewRobotHandler of _onMeetNewRobotCallbacks) {
+                onMeetNewRobotHandler(robot)
+            }
+        }
     }
 }
